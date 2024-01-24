0c867c4c2ecc89147628930a184a230d1fd14fb1
root <none_ssj9@hotmail.com>
2024-01-24 14:37:35 +0000

    Remove Joystick sdL_init for gptokeyb use. Avoids double mapping of inputs

--- src/main.c
+++ src/main.c
@@ -116,7 +116,7 @@ int main (int argc, char *argv[])
     }
 
 
-    if (SDL_Init (SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) < 0)
+    if (SDL_Init (SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
     {
         exit (1);
     }
